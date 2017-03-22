24f8bfff6bb3a0d5e1dbe0fa6b0113ae1ad1ba71
root <root@strongcentosswan.lan>
2017-03-22 11:54:52 +0300

    First commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"bytes"
+	"log"
+	"net/http"
+	"os/exec"
+	"github.com/rs/cors"
+)
+
+// taken from https://github.com/rs/cors
+
+func pissHandler(w http.ResponseWriter, r *http.Request) {
+
+//		w.Header().Set("Access-Control-Allow-Origin", "http://10.0.0.26")
+
+                cmd := exec.Command("strongswan","statusall")
+                var out bytes.Buffer
+                cmd.Stdout = &out
+                err := cmd.Run()
+                if err != nil {
+                        log.Fatal(err)
+                }
+
+		fmt.Fprintln(w, out.String())
+	}
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/piss", pissHandler)
+
+	handler := cors.Default().Handler(mux)
+	log.Fatal(http.ListenAndServe(":9000", handler))
+
+}
