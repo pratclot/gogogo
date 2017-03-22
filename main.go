17c10db31cea7e3c58a2e6f016dd9a3de38eb153
root <root@strongcentosswan.lan>
2017-03-22 17:42:24 +0300

    Lazy commit-message Wed Mar 22 17:42:24 MSK 2017

--- main.go
+++ main.go
@@ -26,10 +26,25 @@ func pissHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, out.String())
 	}
 
+
+func getServerTSHandler(w http.ResponseWriter, r *http.Request) {
+
+		cmd := exec.Command("ip","a")
+                var out bytes.Buffer
+                cmd.Stdout = &out
+                err := cmd.Run()
+                if err != nil {
+                        log.Fatal(err)
+                }
+
+		fmt.Fprintln(w, out.String())
+	}
+
 func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/piss", pissHandler)
+	mux.HandleFunc("/getServerTS", getServerTSHandler)
 
 	handler := cors.Default().Handler(mux)
 	log.Fatal(http.ListenAndServe(":9000", handler))
